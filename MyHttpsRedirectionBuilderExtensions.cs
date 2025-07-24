3f0ca4f05e2eaaf58e19f695b3b36cdba5393655
Jared Kubly <jared.kubly@gmail.com>
2025-07-24 07:34:45 -0600

    initial commit

---
+++ MyHttpsRedirectionBuilderExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Hosting.Server.Features;
+using Microsoft.AspNetCore.HttpsPolicy;
+
+namespace razormy;
+
+public static class MyHttpsRedirectionBuilderExtensions
+{
+    /// <summary>
+    /// Adds middleware for redirecting HTTP Requests to HTTPS.
+    /// </summary>
+    /// <param name="app">The <see cref="IApplicationBuilder"/> instance this method extends.</param>
+    /// <returns>The <see cref="IApplicationBuilder"/> for HttpsRedirection.</returns>
+    public static IApplicationBuilder UseMyHttpsRedirection(this IApplicationBuilder app)
+    {
+        ArgumentNullException.ThrowIfNull(app);
+
+        var serverAddressFeature = app.ServerFeatures.Get<IServerAddressesFeature>();
+        if (serverAddressFeature != null)
+        {
+            app.UseMiddleware<MyHttpsRedirectionMiddleware>(serverAddressFeature);
+        }
+        else
+        {
+            app.UseMiddleware<MyHttpsRedirectionMiddleware>();
+        }
+        serverAddressFeature = app.ServerFeatures.Get<IServerAddressesFeature>();
+        return app;
+    }
+}
---
+++ MyInternalHttpsLoggingExtensions.cs
@@ -0,0 +1,16 @@
+﻿//namespace razormy.extensions;
+
+//internal static partial class MyHttpsLoggingExtensions
+//{
+//    [LoggerMessage(1, LogLevel.Debug, "Redirecting to '{redirect}'.", EventName = "RedirectingToHttps")]
+//    public static partial void MyRedirectingToHttps(this ILogger logger, string redirect);
+
+//    [LoggerMessage(2, LogLevel.Debug, "Https port '{port}' loaded from configuration.", EventName = "PortLoadedFromConfig")]
+//    public static partial void MyPortLoadedFromConfig(this ILogger logger, int port);
+
+//    [LoggerMessage(3, LogLevel.Warning, "Failed to determine the https port for redirect.", EventName = "FailedToDeterminePort")]
+//    public static partial void MyFailedToDeterminePort(this ILogger logger);
+
+//    [LoggerMessage(5, LogLevel.Debug, "Https port '{httpsPort}' discovered from server endpoints.", EventName = "PortFromServer")]
+//    public static partial void MyPortFromServer(this ILogger logger, int httpsPort);
+//}
